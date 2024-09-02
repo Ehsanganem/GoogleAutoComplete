103d4ea9686a5e48daee02bcc4ca77a44a487f59
ofirtzabari <ofirtzabari@gmail.com>
2024-09-02 13:11:21 +0300

    pep8 fix

--- Tests/TestSearchFiles.py
+++ Tests/TestSearchFiles.py
@@ -1,9 +1,9 @@
 from SearchFiles import search_files
 
-SET1 = {1,2,3}
-SET2 = {2,4}
-SET3 = {1,3}
-WORD_DICT = { "hello": SET1, "world": SET2 }
+SET1 = {1, 2, 3}
+SET2 = {2, 4}
+SET3 = {1, 3}
+WORD_DICT = {"hello": SET1, "world": SET2}
 HELLO_WORLD = "hello world"
 HELLO = "hello"
 HELO = "helo"
@@ -14,26 +14,29 @@ def test_search_files():
     res = search_files(HELLO_WORLD, WORD_DICT)
     assert res == [2]
 
+
 def test_search_files2():
     res = search_files(HELLO, WORD_DICT)
-    assert res == [1,2,3]
+    assert res == [1, 2, 3]
 
 
 def test_mistake_in_word():
     res = search_files(HELO, WORD_DICT)
-    assert res == [1,2,3]
+    assert res == [1, 2, 3]
+
 
 def test_mistake_in_word2():
     res = search_files(ORLD, WORD_DICT)
-    assert res == [2,4]
+    assert res == [2, 4]
+
 
 def test_mistake_in_word3():
     res = search_files(HELO+" "+ORLD, WORD_DICT)
     assert res == [2]
-    assert not res == [2,4]
+    assert not res == [2, 4]
+
 
 def test_empty_intersection():
     WORD_DICT[HELLO] = SET3
     res = search_files(HELLO_WORLD, WORD_DICT)
     assert res == []
-
